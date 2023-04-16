5950ac4a49c65919c1cc3632081802b1b8f8c544
SvetAlexa <dazzle@bk.ru>
2023-04-16 17:32:05 +0300

    рефакторинг кода js

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,36 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupOpenButtomElement = document.querySelector('.profile__edit-button');
 
-const openPopup = function () {
-    popupElement.classList.add('popup_is-opened');
-};
-
-const closePopup = function () {
-    popupElement.classList.remove('popup_is-opened');
-};
-
-popupOpenButtomElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__activity');
 
 let formElement = document.querySelector('.popup__container');
 let nameInput = formElement.querySelector('.popup__input_value_name');
 let jobInput = formElement.querySelector('.popup__input_value_activity');
 
+//открываем popup, заполняем поля формы значениями из профиля
+const openPopup = function () {
+    popupElement.classList.add('popup_is-opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+};
+
+//закрываем popup
+const closePopup = function () {
+    popupElement.classList.remove('popup_is-opened');
+};
+
+//обработчики событий открытия и закрытия popup
+popupOpenButtomElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+//обработчик "отправки" формы, заполняем профиль новыми значениями из полей формы
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__activity');
+    evt.preventDefault(); //отменяем стандартную отправку формы
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popupElement.classList.remove('popup_is-opened');
+    closePopup();
 }
 
+//обработчик события "отправки" формы
 formElement.addEventListener('submit', handleFormSubmit);
