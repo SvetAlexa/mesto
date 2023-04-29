2b625ca82c653caf425024a6e36e6a40051a5e92
SvetAlexa <dazzle@bk.ru>
2023-04-29 22:08:38 +0300

    добавлены карточки из коробки

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,48 @@
 const initialCards = [
     {
-        link: 'https://unsplash.com/photos/ec3QSoInblM',
+        link: 'https://images.unsplash.com/photo-1644543419167-2cc7a5738665?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8JUQwJUExJUQwJUI4JUQxJTg1JUQwJUJFJUQxJTgyJUQxJThEJTIwJUQwJTkwJUQwJUJCJUQwJUI4JUQwJUJEJUQxJThDfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
         name: 'Сихотэ-Алинь Приморский край',
     },
     {
-        link: 'https://unsplash.com/photos/1PXSA5d4208',
+        link: 'https://images.unsplash.com/photo-1598535348425-e76a7cc312d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8JUQwJUExJUQxJTgzJUQwJUJCJUQwJUIwJUQwJUJBJUQxJTgxJUQwJUJBJUQwJUI4JUQwJUI5JTIwJUQwJUJBJUQwJUIwJUQwJUJEJUQxJThDJUQwJUJFJUQwJUJEfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
         name: 'Сулакский каньон Дагестан',
     },
     {
-        link: 'https://unsplash.com/photos/wGA5ktslkfc',
+        link: 'https://images.unsplash.com/photo-1660026671516-21658a9c2c7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8JUQwJUI0JUQwJUJFJUQwJUJCJUQwJUI4JUQwJUJEJUQwJUIwJTIwJUQwJUI0JUQxJTgzJUQxJTg1JUQwJUJFJUQwJUIyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
         name: 'Чечкыш (Долина горных духов) Горный Алтай',
     },
     {
-        link: 'https://unsplash.com/photos/XdZCa1M2m9o',
+        link: 'https://images.unsplash.com/photo-1614000531402-74cca389903f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fCVEMCVCMSVEMCVCMCVEMCVCOSVEMCVCQSVEMCVCMCVEMCVCQnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         name: 'Байкал',
     },
     {
-        link: 'https://unsplash.com/photos/sCLNmLYemfA',
+        link: 'https://images.unsplash.com/photo-1634665610480-073d92470559?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fCVEMCVCNCVEMCVCRSVEMCVCQyVEMCVCMSVEMCVCMCVEMCVCOXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         name: 'Домбай Ущелье Аманауз',
     },
     {
-        link: 'https://unsplash.com/photos/bW-ig06IfAg',
-        name: 'Карачаево-Черкесия Сентинский храм ',
+        link: 'https://images.unsplash.com/photo-1627329904799-607897b1eb60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fCVEMCVBRCVEMCVCQiVEMSU4QyVEMCVCMSVEMSU4MCVEMSU4MyVEMSU4MXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        name: 'Карачаево-Черкесия Сентинский храм',
     },
 ];
 
-const itemsContainer = document.querySelector('.elements__lists');
+const initialCardsContainer = document.querySelector('.elements__lists');
+const template = document.querySelector('#element-item-template');
+
+const createCard = (card) => {
+    const li = template.content.querySelector('.element').cloneNode(true);
+
+    li.querySelector('.element__image').src = card.link;
+    li.querySelector('.element__title').textContent = card.name;
+    initialCardsContainer.append(li);
+    return li;
+};
+
+const listCards = initialCards.map(card => {
+    const cardElement = createCard(card);
+    return cardElement;
+});
+
+
 
 
 
